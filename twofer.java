7ad92e8c4b73a5c7d62b7912a10ce264874bb768
Jackie Wen <jzw22@cornell.edu>
2019-03-15 10:25:53 -0400

    added in check for use of optional package

--- twofer.java
+++ twofer.java
@@ -3,6 +3,6 @@ import java.util.Optional;
 
 public class twofer {
     String twofer(String name) {
-        String.format("One for %s, one for me.", Optional.ofNullable(name).orElse("you"));
+        return String.format("One for %s, one for me.", Optional.ofNullable(name).orElse("you"));
     }
 }
