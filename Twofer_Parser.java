210da4f196d1ba66fd8a9b76d1cf59de46467c4a
Jackie Wen <jzw22@cornell.edu>
2019-03-15 11:55:05 -0400

    checks for one instance of a solution for usage of optional

--- Twofer_Parser.java
+++ Twofer_Parser.java
@@ -1,13 +1,16 @@
 package exercism_parser;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
 import com.github.javaparser.JavaParser;
 import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.ImportDeclaration;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.expr.MethodCallExpr;
@@ -16,7 +19,11 @@ import com.github.javaparser.ast.stmt.ReturnStmt;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 
 public class Twofer_Parser {
-		
+	
+	public static CompilationUnit getContent(String flname) throws Exception{
+        return JavaParser.parse(new FileInputStream(flname));
+    }
+	
     /**
      * Simple visitor implementation for visiting MethodDeclaration nodes.
      */
@@ -37,8 +44,11 @@ public class Twofer_Parser {
             	System.out.println("Incorrect number of parameters");
             }
             else{
-            	if(!n.getParameter(0).toString().equals("String name")){
-            		System.out.println("Incorrect parameter type of name");
+            	if(!n.getType().toString().equals("String")){
+            		System.out.println("Incorrect parameter type, should be String");
+            	}
+            	else if(!n.getParameter(0).toString().equals("String name")){
+            		System.out.println("Incorrect parameter name, should be name");
             	}
             }
             
@@ -46,91 +56,134 @@ public class Twofer_Parser {
         }
     }
     
-    private static class ReturnVisitor extends VoidVisitorAdapter<List<String>> {
+    private static class ReturnVisitor extends VoidVisitorAdapter<List<ReturnStmt>> {
+    	
+    	private boolean optionalUse;
+    	
+    	public  ReturnVisitor(boolean optionalUse){
+    		this.optionalUse = optionalUse;
+    	}
+    	
         @Override
-        public void visit(ReturnStmt n, List<String> returnStatements) {
+        public void visit(ReturnStmt n, List<ReturnStmt> returnStatements) {
             //Check what value is being returned
         	
         	System.out.println("Returning: " + n.getExpression().get().toString());
-            returnStatements.add(n.getExpression().get().toString());
+            returnStatements.add(n);
+            
+            if(this.optionalUse){
+            	n.accept(new OptionalMethodVisitor(), null);
+            }
+            
+            
             super.visit(n, returnStatements);
         }
     }
     
     private static class IfVisitor extends VoidVisitorAdapter<Void> {
+    	
+        boolean containsElse = false;
+
         @Override
-        public void visit(IfStmt n, Void arg) {
-            //Check what value is being returned
-        	
-        	System.out.println("If Condition: " + n.getCondition());
-//        	System.out.println(n.getThenStmt()); //prints out content of if-block
+        public void visit(IfStmt ifs, Void v) {
+            if (ifs.getElseStmt() != null) {
+                containsElse = true;
+            }
+        	//System.out.println("If Condition: " + ifs.getCondition());
+        	//System.out.println(n.getThenStmt()); //prints out content of if-block
         	
         	//Returns Optional.empty if there is no else statement
-        	System.out.println("Else statement: ");// + n.getElseStmt());
+        	//System.out.println("Else statement: ");// + n.getElseStmt());
             
-        	n.accept(new ReturnVisitor(), null);
-        	
-            super.visit(n, arg);
+            //super.visit(n, arg);
         }
     }
     
-    private static class MethodCallVisitor extends VoidVisitorAdapter<Void> {
+    private static class OptionalMethodVisitor extends VoidVisitorAdapter<List<MethodCallExpr>> {
         @Override
-        public void visit(MethodCallExpr n, Void arg) {
+        public void visit(MethodCallExpr n, List<MethodCallExpr> arg) {
             // Found a method call
-        	System.out.println("Method name");
-            System.out.println(n.getScope() + " - " + n.getName());
-            System.out.println("Arguments");
-            System.out.println(n.getArguments().toString() + "\n");
+//        	System.out.println("Method name");
+//            System.out.println(n.getScope() + " - " + n.getName());
+//            System.out.println("Arguments");
+//            System.out.println(n.getArguments().toString() + "\n");
+            arg.add(n);
             // Don't forget to call super, it may find more method calls inside the arguments of this method call, for example.
             super.visit(n, arg);
         }
     }
     
-//    private static class ReturnVisitor extends VoidVisitorAdapter<List<String>> {
-//        @Override
-//        public void visit(ReturnStmt returnStmt, List<String> returnedVal) {
-//        	// TODO: insert a println before the return stmt
-//        	System.out.println(returnStmt.toString());
-//        	System.out.println("Returning: " + returnStmt.getExpression().toString());
-//        	returnedVal.add(returnStmt.getExpression().toString());
-//        	super.visit(returnStmt, returnedVal);
-//        }
-//    }
+    private static boolean optionalUse(List<ReturnStmt> returns){
+    	if(returns.size() > 1){
+    		System.out.println("Can be refactored to use less return statements");    		
+        	return false;
+    	}
+    	else{
+    		ReturnStmt n = returns.get(0);
+    		List<MethodCallExpr> returnMethods = new ArrayList<MethodCallExpr>();
+    		n.accept(new OptionalMethodVisitor(), returnMethods);
+    		if(returnMethods.size() == 3){
+    			if(returnMethods.get(0).getScope().get().toString().equals("String") &&
+    					returnMethods.get(0).getNameAsString().equals("format")){
+    				if(!returnMethods.get(0).getArgument(0).toString().equals("\"One for %s, one for me.\"")){
+//    					System.out.println(returnMethods.get(0).getArgument(0).toStringLiteralExpr().get().asString());
+    					return false;
+    				};
+    			}
+    			if(returnMethods.get(1).getScope().get().toString().equals("Optional.ofNullable(name)") &&
+    					returnMethods.get(1).getNameAsString().equals("orElse")){
+    				if(!returnMethods.get(1).getArgument(0).toString().equals("\"you\"")){
+    					return false;
+    				};
+    			}
+    			if(returnMethods.get(2).getScope().get().toString().equals("Optional") &&
+    					returnMethods.get(2).getNameAsString().equals("orNullable")){
+    				if(!returnMethods.get(2).getArgument(0).toString().equals("name")){
+    					return false;
+    				};
+    			}
+    		}
+    		return true;
+    	}
+    }
     
-	public static void main(String [] Args) throws FileNotFoundException{
-		FileInputStream in = new FileInputStream("twofer.java");
-		CompilationUnit cu = JavaParser.parse(in);
+	public static void main(String [] Args) throws Exception{
+		
+		CompilationUnit cu = getContent("twofer.java");
 		
 		//Check that the class name is "twofer"
 		Optional<ClassOrInterfaceDeclaration> classX = cu.getClassByName("twofer");
-		if (!classX.isPresent()){
-			System.out.println("Class is not properly named");
-		}
-		
-		//Check for import statements
-		if(cu.getImports().size() > 0){
-			System.out.println("Simplify code to no imports?");
-		}
-		
-		//Check in the method header what type is being returned and if there's a parameter (name)		
-		cu.accept(new MethodVisitor(), null);
-		System.out.println("\n");
+		System.out.println("Class properly named: " + classX.isPresent());
 		
 		//Check the return statement
-		List<String> returns = new ArrayList<String>();
-		cu.accept(new ReturnVisitor(), returns);
+		List<ReturnStmt> returns = new ArrayList<ReturnStmt>();
+		cu.accept(new ReturnVisitor(false), returns);
 		if(returns.size() < 1){
 			System.out.println("There's no return statement");
 		}
 		System.out.println("\n");
 		
+		//Check for import statements
+		if(cu.getImports().size() > 0){
+			System.out.println("Simplify code to no imports?");
+			if(cu.getImport(0).getName().toString().equals("java.util.Optional")){
+				System.out.println("Used optional properly: " + optionalUse(returns));
+			}
+		}
+		
+		//Check in the method header for what type is being returned and if there's a parameter (name)		
+		cu.accept(new MethodVisitor(), null);
+		System.out.println("\n");
+		
+		
 		//If there are multiple return statements, then you want to see if there's if-else
-		//cu.accept(new IfVisitor(), null);
-		//System.out.println("\n");
+		IfVisitor ifFinder = new IfVisitor();
+		cu.accept(ifFinder, null);
+		System.out.println("Used if-else statement: " + ifFinder.containsElse);
+		System.out.println("\n");
 		
 		//Checks all methods being called
-		cu.accept(new MethodCallVisitor(), null);
+//		cu.accept(new MethodCallVisitor(), null);
 		//If there's a variable in return statement, want to check the values of the variables
 		
 		
