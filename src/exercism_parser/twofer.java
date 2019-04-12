eddee1ca93cd4613a0377720c12514243df2b196
Jackie Wen <jzw22@cornell.edu>
2019-04-11 20:58:36 -0400

    deleted files that weren't in use

--- src/exercism_parser/twofer.java
+++
@@ -1,8 +0,0 @@
-package exercism_parser;
-import java.util.Objects;
-
-class Twofer {
-  String twofer(String name) {
-    return String.format("One for %s, one for me.", Objects.toString(name, "you"));
-  }
-}
